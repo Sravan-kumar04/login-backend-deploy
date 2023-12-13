10d5c39d5829e36d0b0b926a87c0bbcdd4d19e4e
Sravan-kumar04 <tiprisshravan@gmail.com>
2023-12-13 16:13:32 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -4,10 +4,8 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI
+  )
   .then(() => {
     console.log("DB Connected");
   })
